fa13ca776d89da29a457134b0b4eea5bd77b0848
Ahmed Elkerdawy <91023048+ahmedelkerdawy@users.noreply.github.com>
2024-06-13 19:26:44 +0300

    Add files via upload

--- python/lec_1/test_3.py
+++ python/lec_1/test_3.py
@@ -1,1 +1,10 @@
+# program to acess environment variables
 
+import os
+
+print("-------------------------------------------------")
+print(os.environ)
+print("-------------------------------------------------")
+print(os.environ['HOME'])
+print("-------------------------------------------------")
+print(os.environ['PATH'])
