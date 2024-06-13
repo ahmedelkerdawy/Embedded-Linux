73dc84f427f49dd3db83a04bb9f890f2cf51e8ed
Ahmed Elkerdawy <91023048+ahmedelkerdawy@users.noreply.github.com>
2024-06-13 19:25:25 +0300

    Add files via upload

---
+++ python/lec_1/test_2.py
@@ -0,0 +1,10 @@
+# program to test vowel letters
+# owels_char = 'aeiou'
+
+char = 'c'
+if (char == 'a' ) or (char == 'e' ) or (char == 'i' ) or (char == 'o' ) or (char == 'u' ):
+    print(" vowel")
+else:
+    print("not vowel")    
+
+
