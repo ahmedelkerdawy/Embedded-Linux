7509321f5467fb8855d0c63aaa705ac9e07fcac8
Ahmed Elkerdawy <91023048+ahmedelkerdawy@users.noreply.github.com>
2024-07-06 23:54:22 +0300

    Add files via upload

--- python/lec_4/test_1.py
+++ python/lec_4/test_1.py
@@ -1,1 +1,16 @@
+# python program to count number of lines in file
 
+f = open(r"/home/ahmed-elkerdawy/Documents/01_python/tasks/tee","r") 
+fd = f.readlines()
+print(f"number of lines : {len(fd)}")
+
+# python program to count number of words in file
+xl = f.read()
+print(f"number of words : {len(xl.split())}")
+
+
+# python program to write list into file
+students = ["name","age","id","email"]
+fd = f.write(" ".join(students))
+print(fd)
+f.close()
